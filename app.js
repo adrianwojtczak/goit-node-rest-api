a2cc4c6f20f965a7b8f3ead653dd2bbdbd10d94e
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-09-19 17:34:54 +0200

    Add Auth

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import passport from 'passport';
+
+import './config/config-passport.js';
 
 import contactsRouter from './routes/api/contacts.js';
+import usersRouter from './routes/api/users.js';
 
 const app = express();
 
@@ -11,8 +15,10 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(passport.initialize());
 
 app.use('/api/contacts', contactsRouter);
+app.use('/users', usersRouter);
 
 app.use((req, res) => {
 	res.status(404).json({ message: 'Not found' });
