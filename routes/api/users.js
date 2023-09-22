bd78143159fb8a1bf3cfc3de9b32e83294a97475
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-09-22 19:02:35 +0200

    Add avatar functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import Joi from 'joi';
 import jwt from 'jsonwebtoken';
+import gravatar from 'gravatar';
+
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import jimp from 'jimp';
+import multer from 'multer';
+
 import 'dotenv/config';
 
 import auth from '../../middleware/auth.js';
 import User from '../../models/schemas/user.js';
 
 const jwt_secret = process.env.JWT_SECRET;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const AVATARS_FOLDER = path.join(__dirname, '../../public/avatars');
 
 const router = express.Router();
 
@@ -19,6 +31,30 @@ const addUserSchema = Joi.object({
 				'Password must contain at least 8 characters, one uppercase letter, one digit, and one special character.',
 		}),
 	email: Joi.string().email().required(),
+});
+
+const avatarStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'tmp');
+	},
+	filename: (req, file, cb) => {
+		const avatarFileName = `${req.user._id.toString()}${path.extname(file.originalname)}`;
+		cb(null, avatarFileName);
+	},
+});
+
+const upload = multer({
+	storage: avatarStorage,
+	limits: {
+		fileSize: 1024 * 1024,
+	},
+	fileFilter: (req, file, cb) => {
+		if (file.mimetype.startsWith('image/')) {
+			cb(null, true);
+		} else {
+			cb(new Error('Invalid file type. Only images are allowed.'));
+		}
+	},
 });
 
 router.post('/signup', async (req, res, next) => {
@@ -49,6 +85,10 @@ router.post('/signup', async (req, res, next) => {
 	try {
 		const newUser = new User({ email, subscription });
 		newUser.setPassword(password);
+
+		const avatar = gravatar.url(email, { s: '250', d: 'retro' });
+		newUser.avatarURL = avatar;
+
 		await newUser.save();
 		res.status(201).json({
 			status: 'success',
@@ -173,6 +213,34 @@ router.get('/logout', auth, async (req, res, next) => {
 			code: 401,
 			message: `Not authorized`,
 		});
+	}
+});
+
+router.patch('/avatars', auth, upload.single('avatar'), async (req, res) => {
+	try {
+		if (!req.file) {
+			return res.status(400).json({ message: 'No avatar uploaded' });
+		}
+
+		const avatarPath = path.join('tmp', req.file.filename);
+		const avatar = await jimp.read(avatarPath);
+		await avatar.cover(250, 250).writeAsync(avatarPath);
+
+		const newAvatarFileName = `${req.user._id.toString()}.jpg`;
+		const newAvatarPath = path.join(AVATARS_FOLDER, newAvatarFileName);
+		await fs.rename(avatarPath, newAvatarPath);
+
+		const user = await User.findById(req.user._id);
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+		user.avatarURL = `/avatars/${newAvatarFileName}`;
+		await user.save();
+
+		res.status(200).json({ avatarURL: user.avatarURL });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Server error' });
 	}
 });
 
